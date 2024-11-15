fc359a45a8dc5483bddfc90d3a4cc3edc1416edd
Pranab <pranabnewar181@gmail.com>
2024-11-15 17:32:23 +0530

    useEffect removed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import RanderTask from "@/app/components/RanderTask"; // Import RanderTask component
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
